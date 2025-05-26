4acb010e1ff9a58f98f2d80fe4c0f16c7b804a57
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-05-26 20:46:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -542,16 +542,7 @@ async def upload(bot: Client, m: Message):
                             if os.path.exists(f'{name}.jpg'):
                                 os.remove(f'{name}.jpg')         
 
-                 """       
-                elif "youtu" in url: 
-                    try:
-                        await bot.send_photo(chat_id=m.chat.id, photo=photo, caption=cyt)
-                        count += 1
-                    except Exception as e:
-                        await m.reply_text(str(e))
-                        await asyncio.sleep(1)
-                        continue
-                    """
+                 
                 elif ".ws" in url and  url.endswith(".ws"):
                         try : 
                             await helper.pdf_download(f"{api_url}utkash-ws?url={url}&authorization={api_token}",f"{name}.html")
